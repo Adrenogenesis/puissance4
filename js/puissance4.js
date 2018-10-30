a74beb4005cc0706fee78cc040c2647f0b6d5960
scarabator <43741589+scarabator@users.noreply.github.com>
2018-10-30 14:53:43 +0100

    partie commit

--- js/puissance4.js
+++ js/puissance4.js
@@ -1,7 +1,56 @@
-/*
-Puissance 4
-créer tableau
-var x;
-var y;
+const gameArea = document.getElementById("game-area"); // le seul élément du DOM dont on a besoin
+// c'est l'élément parent de nos cases -> on récupérera l'enfant au clique.
 
-*/
+const cellsStatus = [["vide", "vide", "vide"],
+    ["vide", "vide", "vide"],
+    ["vide", "vide", "vide"]];
+
+var joueurs=[];
+var markers=["X","O"];
+var joueurEnCours = 0;
+joueurs[0] = "joueur1";
+joueurs[1] = "joueur2";
+
+
+
+
+
+gameArea.addEventListener("click", function(event) {
+    let clickedCellElm;
+    let clickedCellName;
+    let clickedCellChordX;
+    let clickedCellChordY;
+
+    clickedCellElm = document.getElementById(event.target.id); //enregistre l'élément  enfant du DOM qui a été cliquée.
+    clickedCellName = event.target.id; //renvoie l'id de la cellule cliquée sous forme de string
+    clickedCellChordX = parseInt(clickedCellName[1]); // récupère le 2eme caractère dans l'ID
+    clickedCellChordY = parseInt(clickedCellName[4]); // récupère le 4eme caractère dans l'ID
+    console.log("X : " + clickedCellChordX + ", Y : " + clickedCellChordY);
+    console.log("clickedCell : " + clickedCellName);
+
+
+    cellsStatus[clickedCellChordX-1][clickedCellChordY-1] = joueurs[0]; // modifie le tableau pour stocker la donnée dans la case cliquée
+    partie();
+    console.log(cellsStatus);
+
+    clickedCellElm.innerHTML = markers[joueurEnCours]; // change la case qui viens d'être cliquée (contrairement a ce que j'ai dis
+    // peut être fait aussi avec le clic MAIS n'oubliez pas de stocker l'information dans un tableau car
+    // il faudra utiliser le tableau pour vérifier les conditions de victoire
+    console.log("joueurEnCour",joueurEnCours);
+});
+
+function changeColor(id) {
+    const tabs = document.getElementsByClassName('tab');
+
+    for (let i = 0; i < tabs.length; ++i) {
+        const item = tabs[i];
+        item.style.backgroundColor = (item.id === id) ? "red" : "white";
+    }
+
+}
+
+function partie() {
+    if(joueurEnCours == 0) joueurEnCours = 1;
+    else joueurEnCours = 0;
+}
+
